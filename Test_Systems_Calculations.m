5d680386010626c904d01f4e6aba844906ef3686
Chris DeGrendele <christopher.degrendele@stonybrook.edu>
2018-10-23 17:33:10 -0400

    Fixed issues
    Was plotting Imag on real side. Screen out inf eigen values

--- Test_Systems_Calculations.m
+++ Test_Systems_Calculations.m
@@ -1,19 +1,19 @@
 clear all
-%alpha = -3;
-%beta_1 = -2.2;  %THIS GIVES WEIRD STUFF?
-%beta_2 = 1.5;
+alpha = -3;
+beta_1 = -2.2;  %THIS GIVES WEIRD STUFF?
+beta_2 = 1.5;
 
-alpha = 0.8;
-beta_1 = -.4;
-beta_2 = -1.3;
+%alpha = 0.8;
+%beta_1 = -.4;
+%beta_2 = -1.3;
 
 %Test system 1
-%Hd = [alpha, beta_1;beta_1,-alpha];
-%Hs = [0,beta_2;beta_2,0];
+Hd = [alpha, beta_1;beta_1,-alpha];
+Hs = [0,beta_2;beta_2,0];
 
 %Test System 2
-Hd = [alpha, beta_1;beta_1,alpha];
-Hs = [beta_2,0;0,0];
+%Hd = [alpha, beta_1;beta_1,alpha];
+%Hs = [beta_2,0;0,0];
 
 %Data
 %Hd = [.8,-.4;-.4,-.8];
@@ -22,7 +22,7 @@ Hs = [beta_2,0;0,0];
 
 %Hd = [0];
 %Hs = [-1/2];
-E=[-5,5];
+E=[-10,10];
 
     a = E(1,1);
     b = E(1,2);
@@ -40,12 +40,21 @@ for e = a:.01:b
         end
     end
     
-    %BUILD AV = BVD (GENERALIZED EIGEN VALUE PROBLEM
+    %BUILD AV = BVD (GENERALIZED EIGEN VALUE PROBLEM)
     A = [e*eye(length) - Hd, -Hs; eye(length), zeros(length)];
     B = [Hsdagger, zeros(length); zeros(length), eye(length)];
     [V,D] = eig(A,B); %AV = BVD
 
         
+    %Take the 0 and infinity eigen values out.
+  %  for i = 1:size(D)
+  %      diag =sort( diag(D)' );
+        
+   %     if abs( diag(1,i)  ) < .01
+            
+        
+    %end
+  
         %#Number 1 2d plot of E vs k
     [count, count2] = size(D);
     
@@ -64,19 +73,39 @@ for e = a:.01:b
             
 end
 
+%Build imaginary and real data to plot. 
+%This will plot a point with imag to imag
+%And only purely real to real
+Imag_k=  [];
+Imag_E = [];
+Real_k = [];
+Real_E = [];
+for e = 1:size(Data_x,2)
+    if imag( Data_x(e) ) ~= 0
+        Imag_k = [Imag_k, imag(Data_x(e)) ];
+        Imag_E = [Imag_E, Data_y(e)];
+    elseif imag( Data_x(e) ) == 0
+        Real_k = [Real_k, Data_x(e)];
+        Real_E = [Real_E, Data_y(e)];
+    end
+end
+
+
 %SCREEN OUT 0 , pi, pi/2 for real data
-Real_k = real(Data_x);
-Real_E = Data_y; 
+
 stop=0;
+[Real_k, sortIndex] = sort(Real_k);
+Real_E = Real_E(sortIndex);
 while stop == 0 
-stop = 1;
 for j = 1:size(Real_k,2)
     if size(Real_k,2) < j
         break
     elseif abs( Real_k(j) ) - .01 < 0   %OUT WITH 0 
         Real_k(j) = [];
         Real_E(j) = [];
-        stop=0;
+        Real_k(end) = [];   %Screens out zeros and infty eigen values.
+        Real_E(end) = [];
+        break
     elseif abs (Real_k(j) - pi*.5)  < .01   %OUT WITH PI/2
         Real_k(j) = [];
         Real_E(j) = [];
@@ -85,36 +114,49 @@ for j = 1:size(Real_k,2)
         Real_k(j) = [];
         Real_E(j) = [];
         stop=0;
+    else
+        stop = 1;
     end
 end
 
 end
 
+
+%Sorts data
+[Imag_k, sortIndex] = sort(Imag_k);
+Imag_E = Imag_E(sortIndex);
 %Same as above, copy-pasted, screens out zeros for imag data now
-Imag_k = imag(Data_x);
-Imag_E = Data_y;
 stop=0;
 while stop ==0 
-stop = 1;
 for j = 1:size(Imag_k,2)
+    flag = 0;
     if size(Imag_k,2) < j
+        flag =1; 
         break
-    elseif abs( Imag_k(j) ) - .01 < 0 
+    elseif abs( Imag_k(j) ) < .1
         Imag_k(j) = [];
         Imag_E(j) = [];
-        stop=0;
+        Imag_k(end) = []; %Screens out zeros and corresponding "inifity"
+        Imag_E(end) = []; 
+        flag = 1;
+        break
+    else
+        stop=1;
+    end
+    if flag == 1; %Work around to break out of outer loop and restart process
+        break
     end
 end
-
 end
+
 
 
 figure;
-a=subplot('Position',[.4 .3 .3 .3]) %Position = [Left bottom width height]
+a=subplot('Position',[.4 .3 .3 .3]); %Position = [Left bottom width height];
 scatter(abs(Real_k), Real_E, '.')
 title('Real k v. E')
 hold on
-b=subplot('Position',[.1 .3 .3 .3])
+b=subplot('Position',[.1 .3 .3 .3]);
 set(b,'YTick',[],'XTick',[]);
 scatter( -abs(Imag_k), Imag_E, '.')
 title('Imaginary k v. E')
@@ -126,6 +168,8 @@ Real_k = abs(Real_k);
 input = input('Input rough bounds for the branch point [lower,higher] = ');
 low = input(1,1);
 high = input(1,2);
+
+
 
 %Screen out big / small values for calculating branch point, set them = 0
 for j = 1:size(Imag_E,2)
