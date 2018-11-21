45daff5a16e1b38f2ef074bebbb01ef6c8d3a1db
Chris DeGrendele <christopher.degrendele@stonybrook.edu>
2018-11-21 10:18:34 -0500

    Clean up ~Changed 0 screening for the better

--- Test_Systems_Calculations.m
+++ Test_Systems_Calculations.m
@@ -97,10 +97,10 @@ b = E(1,2);
 %Build imaginary and real data to plot. 
 %This will plot a point with imag to imag
 min_epsilon = 1e-4;
-min_spacing = .02;
+ideal_spacing = .02;
 
 
-[Data_x, Data_y] = amr_KvsE(Hd,Hs,E,min_epsilon, min_spacing);
+[Data_x, Data_y] = amr_KvsE(Hd,Hs,E,min_epsilon, ideal_spacing);
 %And only purely real to real
 
 Imag_k=  [];
@@ -118,100 +118,50 @@ for e = 1:size(Data_x,2)
 end
 
 
-figure;
-ax1=subplot('Position',[.4 .3 .3 .3]); %Position = [Left bottom width height];
-scatter(abs(Real_k), Real_E, '.')
-title('Real k v. E')
-ylim([a,b])
-
-hold on
-ax2=subplot('Position',[.1 .3 .3 .3]);
-set(ax2,'YTick',[],'XTick',[]);
-scatter( -abs(Imag_k), Imag_E, '.')
-title('Imaginary k v. E')
-ylim([a,b])
-
-return %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%%%%%%%%%%%%%CODE WILL STOP HERE%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-
-
-%SCREEN OUT 0 , pi, pi/2 for real data
-
-stop=0;
-[Real_k, sortIndex] = sort(Real_k);
-Real_E = Real_E(sortIndex);
-while stop == 0 
-for j = 1:size(Real_k,2)
-    if size(Real_k,2) < j
-        break
-    elseif abs( Real_k(j) ) - .1 < 0   %OUT WITH 0 
-        Real_k(j) = [];
-        Real_E(j) = [];
-        Real_k(end) = [];   %Screens out zeros and infty eigen values.
-        Real_E(end) = [];
-        stop=0;
-        break
-    elseif abs ( Real_k(j)) == inf
-        Real_k(j) = [];
-        Real_E(j) = [];
-        stop=0;
-    elseif abs (Real_k(j) - pi*.5)  < .01   %OUT WITH PI/2
-        Real_k(j) = [];
-        Real_E(j) = [];
-        stop=0;
-    elseif abs( Real_k(j)  - pi)  < 0.01     %OUT WITH PI
-        Real_k(j) = [];
-        Real_E(j) = [];
-        stop=0;
-    else
-        stop = 1;
-    end
-end
-
-end
 
 
 %Sorts data
-[Imag_k, sortIndex] = sort(Imag_k);
-Imag_E = Imag_E(sortIndex);
-%Same as above, copy-pasted, screens out zeros for imag data now
-stop=0;
-while stop ==0 
-for j = 1:size(Imag_k,2)
-    flag = 0;
-    if size(Imag_k,2) < j
-        flag =1; 
-        break
-    elseif abs( Imag_k(j) ) < .1
-        Imag_k(j) = [];
-        Imag_E(j) = []; 
-        stop=0;
-        flag = 1;
-        break
-    elseif abs ( Imag_k(j) ) == inf
-        Imag_k(j) = [];
-        Imag_E(j) = [];
-        stop=0;
-    else
-        stop=1;
-    end
-    if flag == 1; %Work around to break out of outer loop and restart process
-        break
-    end
-end
-end
- 
+[Real_k, sortIndex] = sort(Real_k);
+Real_E = Real_E(sortIndex);
+
 [Imag_k, sortIndex] = sort(Imag_k);
 Imag_E = Imag_E(sortIndex);
 
-%Imag_k(1) = [];
-%Imag_E(1) = [];
+%Screens out zeros
+i = 1;
+eps_forzero = .01;
+while i < size(Real_k,2)
+    
+    if i > size(Real_k,2) %End of array reached
+        break
+    elseif abs( Real_k(i) ) - eps_forzero < 0
+        Real_E(i) = [];
+        Real_k(i) = [];
+    elseif abs( Real_k(i) ) - eps_forzero > 0
+        i = i+1; %Only step forward if condition holds
+    end
+    
+end
 
-%if Imag_k(end) - Imag_k(end-1) > Imag_k(end-1)
-%    Imag_k(end) = [];
-%    Imag_E(end) = [];
-%end
+% A copy of above, just for Imaginary values
+%maybe make this into a function for cleaner code, but it's only used 
+%twice so idk
+i = 1;
+eps_forzero = .01;
+while i < size(Imag_k,2)
+    
+    if i > size(Imag_k,2) %End of array reached
+        break
+    elseif abs( Imag_k(i) ) - eps_forzero < 0
+        Imag_E(i) = [];
+        Imag_k(i) = [];
+    elseif abs( Imag_k(i) ) - eps_forzero > 0
+        i = i+1; %Only step forward if condition holds
+    end
+    
+end
+    
+
 
 
 figure;
@@ -227,6 +177,13 @@ scatter( -abs(Imag_k), Imag_E, '.')
 title('Imaginary k v. E')
 ylim([a,b])
 
+
+
+return
+%Below is stuff for calculating branch point 
+%--------------------------------------------------------------------------
+%--------------------------------------------------------------------------
+%--------------------------------------------------------------------------
 
 Imag_k = abs(Imag_k);
 Real_k = abs(Real_k);
@@ -302,24 +259,21 @@ Imag_k = Pure_Imag_k;
 Imag_E = Pure_Imag_E;
 
 %Screen out all the zeros (again)
-stop = 0;
-while stop ==0
-stop =1;
-for j = 1:size(Imag_E,2)
-    if size(Imag_k,2) < j
-        stop=0;
+i = 1;
+eps_forzero = .01;
+while i < size(Imag_k,2)
+    
+    if i > size(Imag_k,2) %End of array reached
         break
-    elseif Imag_k(j) == 0 
-        Imag_k(j) = [];
-        Imag_E(j) = [];
-        stop = 0;
-    elseif (Imag_E(j) > high) | (Imag_E(j) < low)
-        Imag_k(j) = [];
-        Imag_E(j) = [];
-        stop = 0;
+    elseif abs( Imag_k(i) ) - eps_forzero < 0
+        Imag_E(i) = [];
+        Imag_k(i) = [];
+    elseif abs( Imag_k(i) ) - eps_forzero > 0
+        i = i+1; %Only step forward if condition holds
     end
+    
 end
-end
+    
 
 %Calculate branch point
 [K_branch,loc] = max(Imag_k);
