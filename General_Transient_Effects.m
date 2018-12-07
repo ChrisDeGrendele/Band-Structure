e665aceb103e9d9522fe7e9ef4356b512de63bde
Chris DeGrendele <christopher.degrendele@stonybrook.edu>
2018-12-07 12:15:46 -0500

    Fixed eigenvalue=1 bug

--- General_Transient_Effects.m
+++ General_Transient_Effects.m
@@ -1,18 +1,22 @@
-
 %A SIMPLE TWO BAND STRUCTURE
-alpha = -1/2;  %Try 0, try -1/2.
+alpha = 0;  %Try 0, try -1/2.
 beta_1 = -.3;
 beta_2 = -1;
-Hd = [alpha, beta_1;beta_1,-alpha];
-Hs = [0,beta_2;0,0];
-E = 1;
+Hd = [alpha, beta_1;beta_1,alpha];
+Hs = [0,beta_2;beta_2,0];
+%E = 0.7 + .000001*i;
+E = 0.7;
 
 [V,D] = buildVDV(Hd,Hs,E);
+%Te = Build_Te(Hd,Hs,E);
+%[V,D] = eig(Te);
+
+
 
 Data_k = [];
 Data_norm = [];
 
-for k = 1:7
+for k = 1:20
     Data_norm = [Data_norm,  norm( V* (D^k) *V^-1 )];
     Data_k = [Data_k, k];
 end
@@ -22,27 +26,27 @@ plot(Data_k, Data_norm)
 %Below is the Ta Tb stuff
 %We're going to have 1 Hd and 2 Hs For an A(BA)^k molecule
 
-Hd = 
-Hs_a = 
-Hs_b = 
+%Hd = 
+%Hs_a = 
+%Hs_b = 
 
-[Va, Da] = buildVDV(Hd, Hs_a, E);
-[Vb, Db] = buildVDV(Hd, Hs_b, E);
+%[Va, Da] = buildVDV(Hd, Hs_a, E);
+%[Vb, Db] = buildVDV(Hd, Hs_b, E);
 
-Data_ABA = [];
-Datak = [];
-T_Ba = (Vb*Db*Vb^-1) * (Va*Da*Va^-1);
-T_A = (Va*Da*Va^-1);
-T_Batok = 1; %Starts at 1
+%Data_ABA = [];
+%Datak = [];
+%T_Ba = (Vb*Db*Vb^-1) * (Va*Da*Va^-1);
+%T_A = (Va*Da*Va^-1);
+%T_Batok = 1; %Starts at 1
 
-for k = 0:8
-    Teff = T_A * T_batok;  
-    Datak = [Datak,k];
-    Data_ABA = [Data_ABA, norm(Teff)];
-    T_batok = T_batok*T_ba; %Each k multiply in a new ab. This saves computing power 
-end
+%for k = 0:8
+%    Teff = T_A * T_batok;  
+%    Datak = [Datak,k];
+%    Data_ABA = [Data_ABA, norm(Teff)];
+%    T_batok = T_batok*T_ba; %Each k multiply in a new ab. This saves computing power 
+%end
 
-plot(Datak, Data_ABA)
+%plot(Datak, Data_ABA)
 
 
 
@@ -66,14 +70,13 @@ function [V1,D1] = buildVDV(Hd, Hs, e)
         V(:,i) = temp(:,I(i));
     end
     
+    
     index1 = size(D_array,2)/2 - rank_HS +1;
     index2 = size(D_array,2)/2 + rank_HS;
     
     %Sets all the data points not in 2r to 0
     for i = 1:length(D_array)
-        if i < index1
-            D_array(i) = 0;
-        elseif i > index2
+        if i < index1 || i > index2
             D_array(i) = 0;
         end
     end
@@ -81,9 +84,8 @@ function [V1,D1] = buildVDV(Hd, Hs, e)
     D = diag(D_array);
     %Screen out all eigen values >1
     for i = 1:length(D)
-        Mag = ( (real( D(i,i) ))^2 + (imag( D(i,i) ))^2 )^(1/2);
-        if Mag >= 1
-            D(i,i) =0;
+        if (real(D(i,i))^2 + imag(D(i,i))^2)^(1/2) > 1.0001
+            D(i,i) = 0;
         end
     end
     
